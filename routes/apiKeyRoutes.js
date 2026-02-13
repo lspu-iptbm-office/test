8491a15929356f5f095d4a7e0c79038ddbb828a4
lspu-iptbm-office <lspuiptbm@gmail.com>
2026-02-13 20:58:35 +0800

    limiter

--- routes/apiKeyRoutes.js
+++ routes/apiKeyRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const apiKeyController = require('../controllers/apiKeyController');
-const apiKeyRegistrationLimiter = require('../middlewares/apiKeyRegistrationLimiter');
+const apiKeyRegistrationLimiter = require('../middleware/apiKeyRegistrationLimiter');
 
 router.post('/', apiKeyRegistrationLimiter, apiKeyController.createAPIKey.bind(apiKeyController));
 
